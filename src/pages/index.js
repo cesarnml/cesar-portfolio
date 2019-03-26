03c4b83e8f9e5caf1c6a4c3cbccb919948a7bfc1
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-26 13:53:25 -0700

    page edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,12 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO
+      title="Home"
+      keywords={[`Cesar Napoleon Mejia Leiva`, `portfolio`, `developer`, `web`]}
+    />
+    <h1>So It Begins</h1>
+    <p>Code more ...</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
